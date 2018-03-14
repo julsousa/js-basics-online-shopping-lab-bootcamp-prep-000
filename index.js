b0855c47c6d1991fd6b6bba8ee3bbf70176a390c
Julie Sousa <juliesousa@yahoo.com>
2018-03-14 19:30:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,23 +21,23 @@ function addToCart(item) {
 function viewCart() {
   var cartItems = [];
   if (cart.length === 0) {
-    console.log('Your shopping cart is empty.')
+    console.log('Your shopping cart is empty.');
 
   } else if (cart.length === 1) {
-    var printOneItem = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
-    console.log(printOneItem)
+    var printOneItem = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`;
+    console.log(printOneItem);
 
   } else if (cart.length === 2) {
-    var printTwoItems = `In your cart, you have ${Object.keys(cart[cart.length-2])} at $${cart[cart.length-2][Object.keys(cart[cart.length-2])]} and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
-    console.log(printTwoItems)
+    var printTwoItems = `In your cart, you have ${Object.keys(cart[cart.length-2])} at $${cart[cart.length-2][Object.keys(cart[cart.length-2])]} and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`;
+    console.log(printTwoItems);
   
     
   } else {
       for (var i = 0; i < cart.length-1; i++) {
-      cartItems.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`)
+      cartItems.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`);
           }
-          var printThreeOrMoreItems = `In your cart, you have${cartItems}, and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
-          console.log(printThreeOrMoreItems)
+          var printThreeOrMoreItems = `In your cart, you have${cartItems}, and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`;
+          console.log(printThreeOrMoreItems);
   }
 }
 
